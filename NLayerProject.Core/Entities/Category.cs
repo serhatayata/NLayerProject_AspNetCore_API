c4dcf2735ffe2c5736164bfa804a355bf1bf4a90
Serhat Ayata <srhtayata@gmail.com>
2021-11-27 21:37:25 +0300

    DataLayer

---
+++ NLayerProject.Core/Entities/Category.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayerProject.Core.Entities
+{
+    internal class Category
+    {
+        public Category()
+        {
+            Products = new Collection<Product>();
+        }
+        public int CategoryID { get; set; }
+        public string Name { get; set; }
+        public virtual ICollection<Product> Products { get; set; }
+        public bool IsDeleted { get; set; }
+
+    }
+}
---
+++ NLayerProject.Core/Entities/Product.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayerProject.Core.Entities
+{
+    internal class Product
+    {
+        public int ProductID { get; set; }
+        public string Name { get; set; }
+        public int Stock { get; set; }
+        public decimal Price { get; set; }
+        public int CategoryID { get; set; }
+        public virtual Category Category { get; set; }
+        public bool IsDeleted { get; set; }
+        public string InnerBarcode { get; set; }
+    }
+}
